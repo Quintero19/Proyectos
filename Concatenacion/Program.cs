188a4ebce3b78f5db102d86eba83eb1e0616377b
Yeison <quinterocalleyeisonestiven123@gmail.com>
2026-04-28 20:43:47 -0500

    Hola

---
+++ Concatenacion/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("-------------------------");
+
+//Concatenación:
+string nombre = "Yeison";
+int edad = 20;
+
+Console.WriteLine("Hola mi nombre es: " + nombre);
+Console.WriteLine("mi edad es: " + edad);
+
+Console.WriteLine($"Nombre de usuario: {nombre}, edad: {edad}");
+Console.ReadKey();
--- Proyectos/Program.cs
+++ Proyectos/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hola a todos");
+Console.WriteLine("Aprendiendo un nuevo lenguaje");
+Console.WriteLine(2026);
+
+//Declaracion de variables y tipos de variables
+string Nombre = "Yeison";
+int Edad = 20;
+bool Estado = true;
+DateTime Fecha = DateTime.Now;
+float Precio = 25.000f;
+decimal Descuento = 10.20m;
+
+Console.WriteLine(Nombre);
+Console.WriteLine(Edad);
+Console.WriteLine(Fecha);
+Console.WriteLine(Descuento);
+Console.WriteLine(Precio);
+Console.WriteLine(Estado);
+
