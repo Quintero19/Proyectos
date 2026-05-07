3302ca3a822de000ff5bfc37342078c6ffa601a3
Yeison <quinterocalleyeisonestiven123@gmail.com>
2026-05-06 23:02:11 -0500

    Continuamos con la practica para elaborar un proyecto

---
+++ Operadores aritmeticos/Program.cs
@@ -0,0 +1,25 @@
+﻿int valor1 = 67, valor2 = 40, valor3 = 37;
+var valor4 = 2.4;
+//Operador de suma
+int suma = valor1 + valor2;
+Console.WriteLine("El resultado de la suma es: " + suma);
+
+//Operador de resta
+int resta = valor1 + suma - valor3;
+Console.WriteLine("El resultado de esta operacion es: " + resta);
+
+//Operador de Multiplicacion
+
+int multiplicacion = valor2 * valor3;
+Console.WriteLine("El resultado de la multiplicacion es: " + multiplicacion);
+
+//Operador de division
+var dividir = valor1 / valor2;
+Console.WriteLine("Resultado de division: " + dividir);
+
+//Operador de resuido
+var porciento = valor2 % valor4;
+Console.WriteLine("Porciento: " + porciento);
+
+
+Console.ReadKey();
