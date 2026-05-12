c4d669cc44c448f96816110ef609f5b2ae7b6b09
Yeison <quinterocalleyeisonestiven123@gmail.com>
2026-05-12 18:01:32 -0500

    Nuevos conceptos

---
+++ Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Runtime.InteropServices;
+
+int[] enteros = new int[5];
+
+enteros[0] = 5;
+enteros[1] = 51;
+enteros[2] = 500;
+enteros[3] = 599;
+enteros[4] = 5676;
+
+Console.WriteLine("Su numero de array es: " + enteros[2]);
+
+int[] numeros = [20, 39, 89, 56, 43, 24, 56,];
+
+string[] nombre = { "Ana", "Elizabeth", "Antonio", "samuel", "juan" };
+//for(int i=0; i<numeros.Length; i++)
+//{
+//    Console.Write(numeros[i]);
+//    Console.Write(", ");
+//}
+Array.Sort(numeros);
+Console.WriteLine($"Numeros organizados de manera ascendente {string.Join(",", numeros)}");
+Array.Sort(nombre);
+Console.WriteLine($"nombres organizados de manera ascendente {string.Join(",", nombre)}");
+Array.Reverse(nombre);
+Console.WriteLine($"array al reves: {string.Join(",", nombre)}");
+Array.Reverse(numeros);
+Console.WriteLine($"array al reves: {string.Join(",", numeros)}");
+int posicion = Array.IndexOf(nombre, "ANA");
+Console.WriteLine(posicion);
+Array.Clear(nombre);
+Console.WriteLine($"array limpio: {string.Join(",", nombre)}");
+Array.Fill(numeros, 99);
+Console.WriteLine($"array llenado con 99: {string.Join(",", numeros)}");
+
---
+++ Bucle Break-Continue/Program.cs
@@ -0,0 +1,23 @@
+﻿// Break and continue to the cicle for 
+for(int  i = 1; i < 10; i++)
+{
+    if(i == 7)
+    {
+        break;
+    };
+    Console.WriteLine("Vueltas: " + i);
+};
+
+//Break and continue to the cicle while
+int f = 0;
+while (f < 10)
+{
+    Console.WriteLine("Num Vueltas: " + f);
+    f++;
+    if (f == 7)
+    {
+        //break;
+        continue;
+
+    };
+};
