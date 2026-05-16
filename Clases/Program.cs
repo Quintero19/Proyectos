650efe12bbcffe49bb6d7cbd2293d97498eb2341
Yeison <quinterocalleyeisonestiven123@gmail.com>
2026-05-15 22:30:53 -0500

    Nuevos metodos

---
+++ Clases/Program.cs
@@ -0,0 +1,21 @@
+﻿class Persona
+{
+    string nombre = "Homero sipmson";
+    int edad = 37;
+    string direccion = "Calle siempre viva n°. 742";
+    int salario = 243;
+    bool estado = true;
+
+
+
+    static void Main()
+    {
+        Persona personal1 = new Persona();
+        Console.WriteLine($"Nombre:{personal1.nombre}\n" +
+            $"Edad:{personal1.edad}\n" +
+            $"Direccion:{personal1.direccion}\n" +
+            $"Salario:{personal1.salario}\n" +
+            $"Estado: {personal1.estado} ");
+    }
+
+}
